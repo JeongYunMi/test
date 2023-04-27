2641ec4002bfeec825346d2fb284361580e25b8f
JeongYunMi <tamd5971@gmail.com>
2023-04-28 00:04:59 +0900

    check

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const route = require('./route.js');
 app.set('view engine', 'ejs'); // (1)
 app.set('views', path.join(__dirname, 'views')); // (2)
 app.use(express.static(path.join(__dirname, 'views')));
+console.log('log: ', express.static(path.join(__dirname, 'views')), path.join(__dirname, 'views'))
 app.use('/', route);
 // 에러 처리 부분
 app.listen(8080, () => {
